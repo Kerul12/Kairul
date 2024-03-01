0f4e219dd3364d72a95ce067ff579508a4155d78
kerul12 <kerulpaujialpares@gmail.com>
2024-03-01 15:08:21 +0700

    Kairul

---
+++ settings.js
@@ -0,0 +1,61 @@
+//base by DGXeon
+//re-upload? recode? copy code? give credit ya :)
+//YouTube: @DGXeon
+//Instagram: unicorn_xeon13
+//Telegram: t.me/xeonbotinc
+//GitHub: @DGXeon
+//WhatsApp: +916909137213
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//owmner v card
+global.ytname = "YT: KAIRU" //ur yt chanel name
+global.socialm = "-" //ur github or insta name
+global.location = "India, Indonesia, Aizawl" //ur location
+
+//new
+global.botname = 'KANIKU' //ur bot name
+global.ownernumber = '6285385264438' //ur owner number
+global.ownername = 'KAIRUL' //ur owner name
+global.websitex = "https://instagrom/khairul_f.a"
+global.wagc = "https://whatsapp.com/channel/0029VaMAp1F6RGJOOolwFD1w"
+global.themeemoji = '🪀'
+global.wm = "KONIKU-MD."
+global.botscript = '-' //script link
+global.packname = "Sticker By"
+global.author = " KAIRUL"
+global.creator = "6285385264438@s.whatsapp.net"
+global.xprefix = '.'
+global.premium = ["6285385264438"] // Premium User
+global.hituet = 0
+
+//bot sett
+global.typemenu = 'v8' // menu type 'v1' => 'v8'
+global.typereply = 'v2' // reply type 'v1' => 'v3'
+global.autoblocknumber = '92' //set autoblock country code
+global.antiforeignnumber = '91' //set anti foreign number country code
+global.welcome = false //welcome/left in groups
+global.anticall = false //bot blocks user when called
+global.autoswview = false //auto status/story view
+global.adminevent = false //show promote/demote message
+global.groupevent = false //show update messages in group chat
+//msg
+global.mess = {
+	limit: 'Your limit is up!',
+	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
+    done: 'Done✓',
+    error: 'Error!',
+    success: 'Here you go!'
+}
+//thumbnail
+global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
